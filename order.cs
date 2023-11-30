cf2556be32b904e3e823070de1fdf640e3556dc8
Zipeta Paul <81578287+Zipeta-prog@users.noreply.github.com>
2023-12-01 01:40:08 +0300

    added work

---
+++ order.cs
@@ -0,0 +1,14 @@
+namespace BookPurchase
+{
+    internal class Book_name {}
+    internal class ID {}
+    internal class Description {}
+
+    public class Order
+    {
+       static void Main(string[] args)
+       {
+        
+       } 
+    }
+}
